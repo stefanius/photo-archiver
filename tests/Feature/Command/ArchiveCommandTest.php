08c71d33671bc03d98c2dd543e92b937e420a371
Stefanius <sgrootveld@gmail.com>
2024-12-26 23:52:46 +0100

    Add some tests

--- tests/Feature/Command/ArchiveCommandTest.php
+++ tests/Feature/Command/ArchiveCommandTest.php
@@ -56,6 +56,54 @@ class ArchiveCommandTest extends TestCase
     }
 
     #[Test]
+    public function it_can_run_the_archive_command_with_the_per_month_strategy()
+    {
+        // When
+        $this->artisan("archive {$this->path} --strategy=per-month")->assertExitCode(0);
+
+        // Then
+        $filesAfter = collect(File::files($this->path));
+        $directoriesAfter = collect(File::directories($this->path));
+        $expectedFilename = base_path('tests/archive/2018-09') . '/IMG_20180928_082102_1.JPG';
+
+        $this->assertFileExists($expectedFilename);
+        $this->assertEquals(0, $filesAfter->count());
+        $this->assertEquals(4, $directoriesAfter->count());
+    }
+
+    #[Test]
+    public function it_can_run_the_archive_command_with_the_per_day_strategy()
+    {
+        // When
+        $this->artisan("archive {$this->path} --strategy=per-day")->assertExitCode(0);
+
+        // Then
+        $filesAfter = collect(File::files($this->path));
+        $directoriesAfter = collect(File::directories($this->path));
+        $expectedFilename = base_path('tests/archive/2018-09-28') . '/IMG_20180928_082102_1.JPG';
+
+        $this->assertFileExists($expectedFilename);
+        $this->assertEquals(0, $filesAfter->count());
+        $this->assertEquals(5, $directoriesAfter->count());
+    }
+
+    #[Test]
+    public function it_can_run_the_archive_command_with_the_per_month_per_day_strategy()
+    {
+        // When
+        $this->artisan("archive {$this->path} --strategy=per-month-per-day")->assertExitCode(0);
+
+        // Then
+        $filesAfter = collect(File::files($this->path));
+        $directoriesAfter = collect(File::directories($this->path));
+        $expectedFilename = base_path('tests/archive/2018-09/2018-09-28') . '/IMG_20180928_082102_1.JPG';
+
+        $this->assertFileExists($expectedFilename);
+        $this->assertEquals(0, $filesAfter->count());
+        $this->assertEquals(4, $directoriesAfter->count());
+    }
+
+    #[Test]
     public function it_cannot_run_the_archive_command_when_the_path_is_ommitted()
     {
         $this->expectException(RuntimeException::class);
